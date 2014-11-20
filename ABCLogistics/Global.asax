c26e01cb88f4039df9715994f7b8e288672013dd
james-driscoll <james.e.m.driscoll@gmail.com>
2014-11-20 12:57:25 +0000

    Added initial project code.

---
+++ ABCLogistics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ABCLogistics.MvcApplication" Language="C#" %>
---
+++ ABCLogistics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ABCLogistics.Startup))]
+namespace ABCLogistics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
