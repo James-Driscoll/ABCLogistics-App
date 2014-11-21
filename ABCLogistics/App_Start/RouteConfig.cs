3913f1022ef4602f9c56baafa23831bf2f85955a
james-driscoll <james.e.m.driscoll@gmail.com>
2014-11-21 00:13:26 +0000

    Modified app_start configuration to default to Order Branches.

--- ABCLogistics/App_Start/RouteConfig.cs
+++ ABCLogistics/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ABCLogistics
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Order", action = "Branches", id = UrlParameter.Optional }
             );
         }
     }
