1ef3bbcf79c938ca47847c1db9aa08e0721acfca
james-driscoll <james.e.m.driscoll@gmail.com>
2014-11-21 00:18:28 +0000

    Added and configured the Order controller.

---
+++ ABCLogistics/Controllers/OrderController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABCLogistics.Data;
+using ABCLogistics.Services;
+using ABCLogistics.Services.Service;
+
+namespace ABCLogistics.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private OrderService _orderService;
+
+        public OrderController()
+        {
+            _orderService = new OrderService();
+        }
+
+        // GET: Order
+        public ActionResult Branches()
+        {
+            return View(_orderService.getOrderBranches());
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
