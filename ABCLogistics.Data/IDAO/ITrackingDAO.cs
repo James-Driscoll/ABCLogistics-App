6581049a9db7850d011ad8c3c0e8900f32415d91
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 19:32:18 +0000

    Added Tracking DAO interface.

---
+++ ABCLogistics.Data/IDAO/ITrackingDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCLogistics.Data.IDAO
+{
+
+    interface ITrackingDAO
+    {
+
+        // CREATE ====================================================================
+        // addTracking
+        void addTracking(Tracking tracking);
+
+        // READ ======================================================================
+        // getTrackings
+        IList<Tracking> getTrackings();
+
+        // getOrderTrackings
+        IList<Tracking> getOrderTrackings(int order);
+
+        // getTracking
+        Tracking getTracking(int id);
+
+        // UPDATE ====================================================================
+        // editTracking
+        void editTracking(Tracking tracking);
+
+        // DELETE ====================================================================
+        // deleteTracking
+        void deleteTracking(Tracking tracking);
+
+    }
+
+}
