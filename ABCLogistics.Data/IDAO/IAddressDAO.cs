ad7895165428d532fe3e53c94d48902b6e46e032
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 19:30:12 +0000

    Added Address DAO interface.

---
+++ ABCLogistics.Data/IDAO/IAddressDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCLogistics.Data.IDAO
+{
+
+    interface IAddressDAO
+    {
+
+        // CREATE ====================================================================
+        // addAddress
+        void addAddress(Address address);
+
+        // READ ======================================================================
+        // getAddresses
+        IList<Address> getAddresses();
+
+        // getAddress
+        Address getAddress(int id);
+
+        // UPDATE ====================================================================
+        // editAddress
+        void editAddress(Address address);
+
+        // DELETE ====================================================================
+        // deleteAddress
+        void deleteAddress(Address address);
+
+    }
+
+}
