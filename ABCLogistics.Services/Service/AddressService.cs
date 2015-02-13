300524547575127a29b5d5c1e0eb5ee71a126393
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 19:34:32 +0000

    Added Address service file.

---
+++ ABCLogistics.Services/Service/AddressService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABCLogistics.Services.IService;
+using ABCLogistics.Data;
+using ABCLogistics.Data.DAO;
+
+namespace ABCLogistics.Services.Service
+{
+
+    public class AddressService : IAddressService
+    {
+
+        private AddressDAO _addressDAO;
+        public AddressService()
+        {
+            _addressDAO = new AddressDAO();
+        }
+
+        // CREATE ===================================================================
+        // addAddress
+        public void addAddress(Address address)
+        {
+            _addressDAO.addAddress(address);
+        }
+
+        // READ =====================================================================
+        // getAddresses
+        public IList<Address> getAddresses()
+        {
+            return _addressDAO.getAddresses();
+        }
+
+        // getAddress
+        public Address getAddress(int id)
+        {
+            return _addressDAO.getAddress(id);
+        }
+
+        // UPDATE ===================================================================
+        // editAddress
+        public void editAddress(Address address)
+        {
+            _addressDAO.editAddress(address);
+        }
+
+        // DELETE ===================================================================
+        // deleteAddress
+        public void deleteAddress(Address address)
+        {
+            _addressDAO.deleteAddress(address);
+        }
+
+    }
+
+}
