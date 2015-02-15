3b2d4e6dc153c4af033e75512c9029b8b93bd35e
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-15 21:12:35 +0000

    Added Tracking service interface.

---
+++ ABCLogistics.Services/IService/ItrackingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABCLogistics.Data;
+
+namespace ABCLogistics.Services.IService
+{
+
+    interface ITrackingService
+    {
+
+        // CREATE ===================================================================
+        // addTracking
+        void addTracking(Tracking tracking);
+
+        // READ =====================================================================
+        // getTrackinges
+        IList<Tracking> getTrackings();
+
+        // getTracking
+        Tracking getTracking(int id);
+
+        // UPDATE ===================================================================
+        // editTracking
+        void editTracking(Tracking tracking);
+
+        // DELETE ===================================================================
+        // deleteTracking
+        void deleteTracking(Tracking tracking);
+
+    }
+}
