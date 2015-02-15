35d773b512449a816b94ff84266e8b739ecdbda9
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-15 21:13:08 +0000

    Added User service interface.

---
+++ ABCLogistics.Services/IService/IUserInterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABCLogistics.Data;
+
+namespace ABCLogistics.Services.IService
+{
+
+    interface IUserService
+    {
+
+        // CREATE ===================================================================
+        // addUser
+        void addUser(User user);
+
+        // READ =====================================================================
+        // getUsers
+        IList<User> getUsers();
+
+        // getUser
+        User getUser(int id);
+
+        // UPDATE ===================================================================
+        // editUser
+        void editUser(User user);
+
+        // DELETE ===================================================================
+        // deleteUser
+        void deleteUser(User user);
+
+    }
+
+}
