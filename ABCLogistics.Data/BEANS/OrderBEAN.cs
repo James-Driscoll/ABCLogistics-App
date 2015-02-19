f253bc19506e159cfe6559d2dfd7c72ae8a04460
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-19 14:24:23 +0000

    Added Order BEAN class.

---
+++ ABCLogistics.Data/BEANS/OrderBEAN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCLogistics.Data.BEANS
+{
+    
+    public class OrderBEAN
+    {
+
+        public int PK_OrderID { get; set; }
+        public int BranchName { get; set; }
+        public int AddressName { get; set; }
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+        public string ItemInsuranceType { get; set; }
+        public string ItemRecordedDelivery { get; set; }
+        public DateTime DateOrdered { get; set; }
+        public DateTime DateDelivered { get; set; }
+        public string Status { get; set; }
+    }
+
+}
