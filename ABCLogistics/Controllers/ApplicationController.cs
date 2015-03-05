836c3b98dcfd64c90730be217a8e459e9df49db8
james-driscoll <james.e.m.driscoll@gmail.com>
2015-03-05 17:01:11 +0000

    Added branchList and added it to view drop down lists.

--- ABCLogistics/Controllers/ApplicationController.cs
+++ ABCLogistics/Controllers/ApplicationController.cs
@@ -20,19 +20,21 @@ namespace ABCLogistics.Controllers
         public ABCLogistics.Services.Service.ParcelService _parcelService;
         public ABCLogistics.Services.Service.TrackingService _trackingService;
         public ABCLogistics.Services.Service.UserService _userService;
+        public ABCLogistics.Services.Service.BranchService _branchService;
 
         // Declare dictionaries.
-        //public Dictionary<int, string> _itemDictionary;
-        //public Dictionary<int, string> _addressDictionary;
         //public Dictionary<int, string> _branchDictionary;
 
         // CONSTRUCTOR ==============================================================
         public ApplicationController()
         {
+            // Construct services.
             _parcelService = new ABCLogistics.Services.Service.ParcelService();
             _trackingService = new ABCLogistics.Services.Service.TrackingService();
             _userService = new ABCLogistics.Services.Service.UserService();
+            _branchService = new ABCLogistics.Services.Service.BranchService();
 
+            // Construct list of Tracking statuses.
             var trackingStatuses = new SelectList(new[] 
             {
                 new { ID = "Processing", Name = "Processing" },
@@ -47,6 +49,7 @@ namespace ABCLogistics.Controllers
             "ID", "Name", 1);
             ViewData["trackingStatuses"] = trackingStatuses;
 
+            // Construct list of Order statuses.
             var orderStatuses = new SelectList(new[] 
             {
                 new { ID = "Received", Name = "Received" },
@@ -55,6 +58,19 @@ namespace ABCLogistics.Controllers
             },
             "ID", "Name", 1);
             ViewData["orderStatuses"] = orderStatuses;
+
+            // Construct list of Branch names.
+            List<SelectListItem> branchList = new List<SelectListItem>();
+            foreach (var item in _branchService.getBranches())
+            {
+                branchList.Add(
+                    new SelectListItem()
+                    {
+                        Text = item.Name,
+                        Value = item.Name
+                    });
+            }
+            ViewBag.branchList = branchList;
 
         }
 
